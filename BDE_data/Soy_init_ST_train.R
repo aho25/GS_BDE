1a4609a8bf84788ea371ddf811c5e0dfc5b3fd00
aho25 <mineevsasha@gmail.com>
2020-04-24 16:27:54 +0300

    Create Soy_init_ST_train.R

---
+++ BDE_data/Soy_init_ST_train.R
@@ -0,0 +1,85 @@
+library(SoyNAM)
+library("rrBLUP")
+Soy_init_ST <- function() {
+  
+  ### Load data
+  data(soybase)
+  dim(gen.qa)
+  dim(data.line.qa)
+  
+  ### Data imputation
+  idx_arr <- rownames(gen.qa) %in% data.line.qa$strain
+  Markers.src = gen.qa[idx_arr,]
+  Markers.src[Markers.src == 0] <- -1
+  Markers.src[Markers.src == 1] <- 0
+  Markers.src[Markers.src == 2] <- 1
+  summary(Markers.src[,1:10])
+  sum(is.na(Markers.src))/nrow(Markers.src)/ncol(Markers.src)
+  impute <- A.mat(Markers.src, min.MAF = 0.05, max.missing = 0.5, impute.method = "mean", return.imputed = T)
+  markers_imputed <- as.data.frame(impute$imputed)
+  dim(markers_imputed)
+  summary(markers_imputed[,1:10])
+  
+  ### Order data
+  idx_arr <- data.line.qa$strain %in% rownames(markers_imputed)
+  Pheno.src <- data.line.qa[idx_arr,]
+  dim(Pheno.src)
+  podmnogestvo <- unique(c(which(is.na(Pheno.src$yield)), which(is.na(Pheno.src$moisture)), which(is.na(Pheno.src$protein)),
+                           which(is.na(Pheno.src$oil)), which(is.na(Pheno.src$fiber)), which(is.na(Pheno.src$size))))  #delete 'NA' from traits
+  Pheno.src <- Pheno.src[-podmnogestvo,]
+  dim(Pheno.src)
+  idx_vec <- match(Pheno.src$strain, rownames(markers_imputed))
+  Markers.src <- markers_imputed[idx_vec,]
+  dim(Markers.src)
+  
+  ### Location: 'NE', Year: 2012
+  USED_YEAR = c(2012)
+  podmnogestvo <- Pheno.src$year %in% USED_YEAR
+  Pheno <- Pheno.src[podmnogestvo,]
+  Markers <- Markers.src[podmnogestvo,]
+  USED_LOCATION = c('NE')
+  podmnogestvo <- Pheno$location %in% USED_LOCATION
+  Pheno <- Pheno[podmnogestvo,]
+  Markers <- Markers[podmnogestvo,]
+  dim(Markers)
+  dim(Pheno)
+  
+  Markers.2012.NE <- Markers
+  Pheno.2012.NE <- Pheno
+  
+  ### Initialize data
+  n <- nrow(Markers)
+  p <- ncol(Markers)
+  probedata <- sample(1:n, 0.8 * n)
+  validata <- setdiff(1:n, probedata)
+  p.valid <- as.matrix(Pheno[validata,]$yield)
+  m.valid <- Markers[validata,]
+  p.probe <- as.matrix(Pheno[probedata,]$yield)
+  m.probe <- Markers[probedata,]
+  # 
+  ### Check
+  set.seed(12)
+  m.rows = sample(n,300)
+  m.cols = sample(p,300)
+  m.probe = Markers[m.rows,m.cols]
+  p.probe = as.matrix(Pheno[m.rows,]$yield)
+  MARKERS = m.probe
+  PHENO = p.probe
+  View(m.probe)
+  Init_data <- list(p.valid=p.valid, m.valid=m.valid, p.probe=p.probe, m.probe=m.probe)
+  return(Init_data)
+}
+
+new.probe<-as.matrix(m.probe)
+new.data<-as.matrix(dnorm(x=0, mean = 0, sd = 1, log = FALSE))
+new.data
+
+x=seq(-5,5,by=0.1);
+y<-as.matrix(pnorm(x,mean=-1,sd=2), nrow=299, ncol=1)
+y1<-as.matrix(pnorm(x,mean=-1,sd=2), nrow=299, ncol=1)
+y2<-as.matrix(pnorm(x,mean=-1,sd=2), nrow=299, ncol=1)
+y3<-rbind(y,y1)
+y3<-rbind(y3,y2)
+x<-c(301,302, 303)
+y4<-y3[-x,]
+new.vector<-m.probe*y4
