7c3fd3d6fa582dc5e1371e280405fa4a5d984f93
aho25 <mineevsasha@gmail.com>
2020-05-21 13:27:43 +0300

    Update Pheno2_synthetic_ST.R

--- BDE_data/Pheno2_synthetic_ST.R
+++ BDE_data/Pheno2_synthetic_ST.R
@@ -1,5 +1,9 @@
+#pheno2_syntetic
+### Set noise coefficient
+k <- 0.2
+
 ### Download markers
-MARKERS <- read.csv('data_csv/markers_synthetic.csv')
+MARKERS <- read.csv('data_csv/synthetic/markers_synthetic.csv')
 rownames(MARKERS) <- MARKERS[,1]
 MARKERS <- MARKERS[,-1]
 
@@ -8,23 +12,24 @@ set.seed(12)
 major_len_1 <- 20
 major_ind_1 <- sample(ncol(MARKERS), major_len_1)
 weight_1 <- rep(0,ncol(MARKERS))
-weight_1[major_ind_1] <- weight_1[major_ind_1] + rnorm(major_len_1, 10, 1)#is variance can makes a difference to results?
+weight_1[major_ind_1] <- weight_1[major_ind_1] + rnorm(major_len_1, 10, 1)
 names(weight_1) <- colnames(MARKERS)
 major_snp_1 <- names(sort(weight_1[major_ind_1], decreasing = T))
 priznak_1 <- as.vector(crossprod(t(MARKERS),weight_1))
-PHENO <- priznak_1 + 2*abs(min(priznak_1))
-PHENO <- PHENO + rnorm(nrow(MARKERS), 0, 1)*mean(PHENO)*0.1#create datasets with different variance (0.01; 0.1 )
+PHENO <- priznak_1
+PHENO <- PHENO + rnorm(nrow(MARKERS), 0, 1)*k
 # max(PHENO)
 # min(PHENO)
 # mean(PHENO)
 # scale_1 <- 1
 # PHENO <- PHENO * scale_1
 
-write.csv(PHENO, file = 'data_csv/pheno2_synthetic_st_01.csv')
-write.csv(major_snp_1, file = 'data_csv/major_snp1_synthetic_st_01.csv')
+write.csv(PHENO, file = paste0('data_csv/synthetic/pheno_synthetic_st_', k, '.csv'))
+write.csv(weight_1, file = paste0('data_csv/synthetic/weight_1_synthetic_st_', k, '.csv'))
+write.csv(major_snp_1, file = paste0('data_csv/synthetic/major_snp_1_synthetic_st_', k, '.csv'))
 
+### Control_discrete
 # #predict marker effects
 # ans <- mixed.solve(PHENO,Z=MARKERS)
 # best_features <- rownames(sort(ans$u, decreasing = T))
 # best_features_len <- length(which(best_features[1:20] %in% DATA$major_snp_1))
-
